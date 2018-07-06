c609ce882e96afd617354043d8b070efdc28c95e
Guilherme Conti <guibasconti@gmail.com>
2018-07-06 03:45:22 -0300

    Enalbe cors

--- tools/serverProduction.js
+++ tools/serverProduction.js
@@ -5,10 +5,12 @@ dotenv.config();
 
 const express = require('express');
 const router = require('../server/core/router.js');
+const cors = require('cors');
 const logger = require('./logger');
 
 const app = express();
 
+app.use(cors());
 app.use('/', router);
 app.use(logger.errorHandler());
 

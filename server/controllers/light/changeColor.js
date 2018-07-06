944d24443b580e1ee16ef25638fb20383a8a301c
Guilherme Conti <guibasconti@gmail.com>
2018-07-06 03:06:18 -0300

    True colors API

--- server/controllers/light/changeColor.js
+++ server/controllers/light/changeColor.js
@@ -1,7 +1,28 @@
 const port = require('../../core/serialPort');
 
 module.exports = (req, res) => {
-  port.write('Pexenegue', err => {
+
+  let { red, green, blue } = req.body;
+
+  if (!isValidColor(red)) {
+    return res.status(400).json({
+      msg: 'Invalid red color.'
+    });
+  }
+
+  if (!isValidColor(green)) {
+    return res.status(400).json({
+      msg: 'Invalid green color.'
+    });
+  }
+
+  if (!isValidColor(blue)) {
+    return res.status(400).json({
+      msg: 'Invalid blue color.'
+    });
+  }
+
+  port.write(formatColor(red) + formatColor(green) + formatColor(blue), err => {
     if (err)
       return res.status(500).json({
         msg: err
@@ -11,3 +32,13 @@ module.exports = (req, res) => {
     });
   });
 };
+
+function isValidColor(color) {
+  return color >= 0 && color <= 255;
+};
+
+function formatColor(color) {
+  let formattedColor = color.toString();
+  formattedColor = '0'.repeat(3 - formattedColor.length) + formattedColor;
+  return formattedColor;
+}
